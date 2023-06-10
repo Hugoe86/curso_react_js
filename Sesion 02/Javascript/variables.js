b4b2bce9c8e1bb8b0857ffdf261256e36082440a
Hugo Ramirez Aguilera <huguillo_era@hotmail.com>
2023-06-10 15:52:37 -0600

    sesion #2

---
+++ Sesion 02/Javascript/variables.js
@@ -0,0 +1,11 @@
+const pizza = true;
+pizaa = false;
+
+var topic = "JavaScript";
+
+if (topic) {
+    let topic = "React";
+    console.log("block", topic);//  React
+}
+
+console.log("Global", topic);// JavaScript
