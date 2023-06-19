ee70225bef05e1dd0e0d4d1efd8501d33818c2c0
Hugo Enrique Ramírez Aguilera <huguillo_era@hotmail.com>
2023-06-19 09:26:40 -0600

    clase 03
    estos son los archivos que se crearon durante la clase 03

---
+++ Sesion 03/ejercicio01/src/components/ProductItem.jsx
@@ -0,0 +1,13 @@
+const ProductItem = ({ product }) => {
+    const { name, description, price } = product
+    return (
+      <div className={`p-4  rounded-xl ${product.price < 500 ? 'bg-slate-200' : 'bg-yellow-200'}`}>
+        <h2>{name}</h2>
+        <p>{description}</p>
+        <p>${price}</p>
+      </div>
+    );
+  }
+
+
+  export default ProductItem;
