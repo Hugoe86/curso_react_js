0833efd2a81eb9de76b74743d1ef7886a54c4f0f
Hugo Enrique Ramirez Aguilera <enrique.ramirez@contel.com.mx>
2023-07-03 08:39:57 -0600

    Evento para que se refrescen los botones en tiempo real

--- Sesion 05/Proyecto/fakestore/src/components/ProductItem.jsx
+++ Sesion 05/Proyecto/fakestore/src/components/ProductItem.jsx
@@ -14,6 +14,29 @@ const ProductItem = ({ product }) => {
   } = useCartContext();
 
   const validar_producto = () => {
+    
+    //console.log(product);
+    let valores = cart.filter(
+      (product_filtro) => product_filtro.id == product.id
+    );
+
+    //  si la longitud del filtro es 0 no se encuenra en el carrito
+    if (valores.length == 0) {
+      setshowAdd(true);
+    }
+    //  de lo contrario el producto esta en el carrito
+    else {
+      setshowAdd(false);
+    }
+
+    console.log(valores.length);
+    console.log(valores);
+  };
+
+  const onSubmit = (event) => {
+    
+    event.preventDefault();
+
     //console.log(product);
     let valores = cart.filter(
       (product_filtro) => product_filtro.id == product.id
@@ -39,37 +62,40 @@ const ProductItem = ({ product }) => {
   }, [product]);
 
   return (
-    <div className="flex flex-col border border-gray-300 shadow-sm rounded-xl p-4">
-      <img
-        src={product.image}
-        alt={product.title}
-        className="h-28 object-cover self-center"
-      />
-      <h2 className="font-bold">{product.title}</h2>
-      <span>${product.price}</span>
+    <form onSubmit={onSubmit}>
+      <div className="flex flex-col border border-gray-300 shadow-sm rounded-xl p-4">
+        <img
+          src={product.image}
+          alt={product.title}
+          className="h-28 object-cover self-center"
+        />
+        <h2 className="font-bold">{product.title}</h2>
+        <span>${product.price}</span>
 
-      {showAdd ? (
-        <button
-          className="bg-black hover:bg-gray-800 text-white rounded-md p-2 mt-2"
-          onClick={() => {
-            dispatch({ type: "ADD_TO_CART", payload: product });
-            alert("Producto añadido al carrito");
-          }}
-        >
-          Añadir al carrito
-        </button>
-      ) : (
-        <button
-          className="bg-red-500  hover:bg-red-800 text-white rounded-md p-2 mt-2"
-          onClick={() => {
-            dispatch({ type: "REMOVE_FROM_CART", payload: product });
-            alert("Producto eliminado del carrito");
-          }}
-        >
-          Eliminar del carrito
-        </button>
-      )}
-    </div>
+        {/* validamo que boton se mostrara, si es VERDADERO se mostrara el boton de agregar, si es FALSO mostrara boton de eliminar*/}
+        {showAdd ? (
+          <button
+            className="bg-black hover:bg-gray-800 text-white rounded-md p-2 mt-2"
+            onClick={() => {
+              dispatch({ type: "ADD_TO_CART", payload: product });
+              alert("Producto añadido al carrito");
+            }}
+          >
+            Añadir al carrito
+          </button>
+        ) : (
+          <button
+            className="bg-red-500  hover:bg-red-800 text-white rounded-md p-2 mt-2"
+            onClick={() => {
+              dispatch({ type: "REMOVE_FROM_CART", payload: product });
+              alert("Producto eliminado del carrito");
+            }}
+          >
+            Eliminar del carrito
+          </button>
+        )}
+      </div>
+    </form>
   );
 };
 
