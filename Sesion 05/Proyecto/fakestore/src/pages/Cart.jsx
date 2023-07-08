72ad05eefe8973a7c560ea6cc61c9576c7131c56
Hugo Enrique Ramirez Aguilera <enrique.ramirez@contel.com.mx>
2023-07-08 11:48:43 -0600

    Formato a total del carrito

--- Sesion 05/Proyecto/fakestore/src/pages/Cart.jsx
+++ Sesion 05/Proyecto/fakestore/src/pages/Cart.jsx
@@ -6,6 +6,10 @@ const Cart = () => {
     state: { cart, totalPrice, contador },
   } = useCartContext();
 
+  //  funcion que da formato a un numero
+  function formatNumbre(numbre) {
+    return new Intl.NumberFormat().format(numbre);
+  }
 
   return (
     <div>
@@ -16,7 +20,7 @@ const Cart = () => {
             <CartItem key={index} product={product} />
           ))}
         </div>
-        <div className="font-bold text-3xl">TOTAL: ${totalPrice}</div>
+        <div className="font-bold text-3xl">TOTAL: ${formatNumbre(totalPrice)}</div>
       </div>
     </div>
   );
