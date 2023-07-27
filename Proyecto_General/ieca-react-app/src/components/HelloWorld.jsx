ec1c2a782147b09f7ed38be5cf991f60790db1aa
Hugo Enrique Ramirez Aguilera <enrique.ramirez@contel.com.mx>
2023-07-27 14:07:19 -0600

    ajustes

--- Proyecto_General/ieca-react-app/src/components/HelloWorld.jsx
+++
@@ -1,9 +0,0 @@
-const HelloWorld = () => {
-  return (
-    <div className="bg-white flex justify-center items-center w-full flex-1">
-      <h1 className="text-purple-800 text-3xl">Hello World</h1>
-    </div>
-  );
-};
-
-export default HelloWorld;
